d717925c32932e2c0f514487de45bb2c61b86876
manecci <pmanecci@gmail.com>
2022-01-26 20:42:23 -0500

    Login.js functions and connection to login.html & account.html

---
+++ public/javascript/login.js
@@ -0,0 +1,67 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+    const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+        email,
+        password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage/');
+    } else {
+        alert(response.statusText);
+    }
+    }
+}
+var password = document.getElementById("password-signup")
+    , confirm_password = document.getElementById("password-confirm");
+
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+    confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+    const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+        username,
+        email,
+        password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage/');
+    } else {
+        alert(response.statusText);
+    }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
