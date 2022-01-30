c4139fe06a7babca17e29a5fd2462edbb5af62c9
coleleg <coleleg@gmail.com>
2022-01-29 19:36:56 -0500

    added authentication for user session

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,10 +1,8 @@
-const { response } = require("express");
-
 async function newFormHandler(event) {
     event.preventDefault();
 
-    // const title = document.querySelector('input[name='post-title']).value ---- NOT SURE IF PATTY WILL CALL THIS "post-title" - may need to change
-    // const discord_url = document.querySelector('input[name='discord-url']).value ---- NOT SURE IF PATTY WILL CALL THIS "discord-url" - may need to change
+    const title = document.querySelector('input[name="post-title"]').value;
+    const discord_url = document.querySelector('input[name="discord-url]').value;
 
     const response = await fetch('/api/posts', {
         method: 'POST',
@@ -19,7 +17,7 @@ async function newFormHandler(event) {
 
     if (response.ok) {
         // confirm this name is homepage!!
-        // document.location.replace('/homepage');
+        document.location.replace('/homepage');
     } else {
         alert(response.statusText);
     }
