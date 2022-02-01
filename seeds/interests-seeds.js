1190b050d53f50b394d78830eedaa5caaaa1b963
CTripleF <cjfabra1@gmail.com>
2022-01-31 19:13:23 -0500

    seed fixes

--- seeds/interests-seeds.js
+++ seeds/interests-seeds.js
@@ -1,10 +1,58 @@
-const sequelize = require('../canfig/connection');
+const sequelize = require('../config/connection');
 const {Interest} = require ('../models');
 
 const interestData = [
   {
-
+    game_title:"WoW",
+    game_console: "PC",
+    game_type:"MMO"
+  },
+  {
+    game_title:"Call of Duty",
+    game_console: "XBOX",
+    game_type:"FPS"
+  },
+  {
+    game_title:"Call of Duty",
+    game_console: "PC",
+    game_type:"FPS"
+  },
+  {
+    game_title:"Call of Duty",
+    game_console: "Playstation",
+    game_type:"FPS"
+  },
+  {
+    game_title:"Fortnite",
+    game_console: "Cross-Platform",
+    game_type:"Battle Rayale"
+  },
+  {
+    game_title:"League of Legends",
+    game_console: "PC",
+    game_type:"MOBA"
+  },
+  {
+    game_title:"Star Craft",
+    game_console: "PC",
+    game_type:"RTS"
+  },
+  {
+    game_title:"Magic the Gathering",
+    game_console: "PC",
+    game_type:"Card game"
+  },
+  {
+    game_title:"Dungeons and Dragons",
+    game_console: "PC",
+    game_type:"Table Top"
+  },
+  {
+    game_title:"Dungeons and Dragons",
+    game_console: "PC",
+    game_type:"Table Top"
   }
+
 ]
 
 const seedInterests = () => Interest.bulkCreate(interestData);
