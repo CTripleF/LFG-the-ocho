352795fa46529852ee2d7f1d2e55d5364e47c0af
CTripleF <cjfabra1@gmail.com>
2022-02-01 19:40:41 -0500

    adding back commented code

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,77 +1,77 @@
-// const router = require('express').Router();
-// const sequelize = require('../config/connection');
-// const { Post, User, Interest, Comment } = require('../models');
-// const withAuth = require('../utils/auth');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Interest, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-// // Get your posts
-// router.get('/,', withAuth, (req, res) => {
-//     console.log(req.session);
-//     Post.findAll({
-//         where: {
-//             user_id: req.session.user_id
-//         },
-//         attributes: [
-//             'id',
-//             'title',
-//             'discord_link',
-//             'game_title',
-//             'game_console',
-//             'game_type'
-//         ],
-//         include: [
-//             {
-//                 model: Comment,
-//                 attributes: ['comment_text', 'user_id', 'post_id'],
-//                 include: {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             },
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     }).then(dbPostData => {
-//         const posts = dbPostData.map(post => post.get({ plain: true }));
-//         res.render('profile', { posts, loggedIn: true });
-//     }).catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
+// Get your posts
+router.get('/,', withAuth, (req, res) => {
+    console.log(req.session);
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'discord_link',
+            'game_title',
+            'game_console',
+            'game_type'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['comment_text', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('profile', { posts, loggedIn: true });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-// // Get your interests
-// router.get('/', (req, res) => {
-//     Interest.findAll({
-//         where: {
-//             user_id: req.session.user_id
-//         },
-//         attributes: [
-//             'id',
-//             'post_id',
-//             'game_interest',
-//             'game_title',
-//             'game_console',
-//             'game_type'
-//         ],
-//         include: [
-//             {
-//                 model: Comment
-//             },
-//             {
-//                 model: User
-//             },
-//             {
-//                 model: Post
-//             }
-//         ]
-//     })
-//     .then(dbPostData => res.json(dbPostData))
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
+// Get your interests
+router.get('/', (req, res) => {
+    Interest.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_id',
+            'game_interest',
+            'game_title',
+            'game_console',
+            'game_type'
+        ],
+        include: [
+            {
+                model: Comment
+            },
+            {
+                model: User
+            },
+            {
+                model: Post
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-// module.exports = router;
+module.exports = router;
