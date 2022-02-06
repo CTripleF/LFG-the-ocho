3769ebb30e751af15ecd23076d397b41172c11d7
CTripleF <cjfabra1@gmail.com>
2022-02-06 10:35:11 -0500

    merging sarah

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -36,7 +36,7 @@ async function updateFormHandler(event) {
 // this button is on /profile
 function editButtonHandler(event) {
     event.preventDefault();
-    res.render('profile-edit');
+    document.location.replace('./profile')
 };
 
 // this button is on /dashboard
